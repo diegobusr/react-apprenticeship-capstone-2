4bc0855dad902ad1c5cc70afdb17cf31a2fcbfdf
Diego Bustamante <diego.bustamante@wizeline.com>
2022-01-25 09:26:44 -0700

    feature: added components structure, eslint and prettier

---
+++ src/components/App/App.js
@@ -0,0 +1,8 @@
+import { GlobalStyle } from '../../global.styles';
+import './App.css';
+
+const App = () => {
+  return <GlobalStyle></GlobalStyle>;
+};
+
+export default App;
---
+++ src/pages/Home/Home.page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomePage() {
+  return <section></section>;
+}
+
+export default HomePage;
