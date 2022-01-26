71bac3112d436f564aeac00bcbea62284dfc18d4
Diego Bustamante <diego.bustamante@wizeline.com>
2022-01-26 15:01:37 -0700

    fix: explanation styles

--- src/components/MediaContent/Explanation/Explanation.styles.js
+++ src/components/MediaContent/Explanation/Explanation.styles.js
@@ -4,8 +4,8 @@ export const StyledSpan = styled.span`
   font-size: 0.8em;
   text-align: justify;
   margin: 3px 3px 3px 3px;
-  color: white;
-  text-shadow: 1px 1px 0px #000000;
+  color: black;
+  text-shadow: 1px 1px 30px white;
   ${(prop) =>
     !prop.expandTextButton &&
     css`
